6730ec5a6c044c308ec54b12fa8f624b66c3865a
Maaike de Jong <psymad@nottingham.ac.uk>
2019-02-27 15:07:17 +0000

    Add PlayerCharacter and simple MasterScript

---
+++ G54SPM Game/Assets/MasterScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterScript : MonoBehaviour {
+
+    // Variable to keep track of whether the gam e is currently in its set up or playing state.
+    // If set to true, the game's objects will move and the player character will be affected by gravity.
+    public bool playingState = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Below commented out code changes value of playingState when space is pressed.
+        // Refactor for proper implementation of state change, including a level reset when it
+        // cycles back from true to false. 
+
+       /* if (Input.GetKeyDown("space"))
+        {
+            if (playingState)
+            {
+                playingState = false;
+            }
+            else
+            {
+                playingState = true;
+           }
+        } */
+    }
+
+
+}
---
+++ G54SPM Game/Assets/TestScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private MasterScript masterScript;
+    private GameObject gameController;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        gameController = GameObject.Find("GameController");
+        masterScript = gameController.GetComponent<MasterScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (masterScript.playingState)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+        if (!masterScript.playingState)
+        {
+            rb.bodyType = RigidbodyType2D.Static;
+        }	
+	}
+}
