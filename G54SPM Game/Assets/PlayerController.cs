73febf6b614d90728ebf96d1fd5e3fdfe757a5fd
Edward <47630437+EdwardDurrans@users.noreply.github.com>
2019-03-06 15:06:48 +0000

    Added "Level 1" text to the top right corner.
    Added "Level 1" text to the top right corner.

--- G54SPM Game/Assets/PlayerController.cs
+++ G54SPM Game/Assets/PlayerController.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     private Rigidbody2D rb;
     private MasterScript masterScript;
     private GameObject gameController;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         gameController = GameObject.Find("GameController");
         masterScript = gameController.GetComponent<MasterScript>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (masterScript.playingState)
         {
             rb.bodyType = RigidbodyType2D.Dynamic;
@@ -28,7 +31,5 @@ public class PlayerController : MonoBehaviour {
 
         // Define boundary of level within the MasterScript, then have the player detect whether it's beyond those bounds.
         // If it is, reset the scene. Using the following line of code: Application.LoadLevel(Application.loadedLevel);
-
-
     }
 }
