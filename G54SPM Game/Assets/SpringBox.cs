495fbda3983cd834280a1d1ba6cc6f57d57bfdf7
Edward <47630437+EdwardDurrans@users.noreply.github.com>
2019-03-13 22:04:08 +0000

    Made level 4

---
+++ G54SPM Game/Assets/SpringBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringBox : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        collider.attachedRigidbody.AddForce(new Vector2(0, 20), ForceMode2D.Impulse);
+        Debug.Log("SpringBox hit");
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
