fd179ab1ef61d3304ec50759ead6c6ac8c24b7e4
Edward <47630437+EdwardDurrans@users.noreply.github.com>
2019-03-20 15:27:31 +0000

    Added comments to StarFadeBox Script

--- G54SPM Game/Assets/StarFadeBox.cs
+++ G54SPM Game/Assets/StarFadeBox.cs
@@ -4,44 +4,47 @@ using UnityEngine;
 
 public class StarFadeBox : MonoBehaviour
 {
-    public int lowerLimitInk;
-    public int upperLimitInk;
+    public int lowerLimitInk; // Integer corresponding to the amount of ink used when the star will start fading
+    public int upperLimitInk; /* Integer corresponding to the amount of ink used when the star will have fully faded
+    and will turn completely white*/
 
-    private float fullBoxHeight;
+    private float fullStarHeight;
     private int totalPointsUsed;
 
     void Start ()
     {
-        fullBoxHeight = transform.localScale.y;
+        fullStarHeight = transform.localScale.y;
         transform.localScale = new Vector2(transform.localScale.x, 0);
 	}
 
 	void Update ()
     {
-        totalPointsUsed = 0;
+        totalPointsUsed = 0; // Reset total points used, this will need to be recalculated each frame
 
         Line[] lines = FindObjectsOfType<Line>();
         foreach (Line line in lines)
         {
-            totalPointsUsed += line.getNumPoints();
+            totalPointsUsed += line.getNumPoints(); // Recalculate total points used
         }
 
         if(totalPointsUsed > upperLimitInk)
         {
-            transform.localScale = new Vector2(transform.localScale.x, fullBoxHeight);
-            GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 1);
+            transform.localScale = new Vector2(transform.localScale.x, fullStarHeight); // Cover star full height
+            GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 1); // Set cover to fully white
         }
 
         else if(totalPointsUsed > lowerLimitInk)
         {
-            float boxHeight = (totalPointsUsed - lowerLimitInk) * fullBoxHeight / (upperLimitInk - lowerLimitInk);
+            float coverStarScale = (totalPointsUsed - lowerLimitInk) * fullStarHeight / (upperLimitInk - lowerLimitInk);
+            // Cover star scale linearly proportional to the amount of ink used
 
-            transform.localScale = new Vector2(transform.localScale.x, boxHeight);
+            transform.localScale = new Vector2(transform.localScale.x, coverStarScale);
         }
 
         else
         {
             transform.localScale = new Vector2(transform.localScale.x, 0);
+            // If the amount of ink used is under lowerLimitInk, then the cover star does not appear
         }
 	}
 }
